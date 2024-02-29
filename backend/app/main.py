f7e4874f1c475b169a44a8aa5260d9d64ad6e6c4
Hwuiwon Kim <hwuiwon.kim@gmail.com>
2024-02-28 23:38:21 -0500

    Add DynamoDBService and setup post endpoint (#4)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 import sys
 from http import HTTPStatus
 
+from api.v1 import post_router
 from exceptions import SummitException
 from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
@@ -67,3 +68,4 @@ def summit_openapi() -> dict:
 
 
 app.openapi = summit_openapi
+app.include_router(post_router, prefix="/v1")
